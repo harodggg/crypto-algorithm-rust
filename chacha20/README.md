5ae856f393b68eb5968c85bb053a26a4363100ae
harodggg <haroldtiansheng@gmail.com>
2024-04-13 15:03:32 +0800

    feat: add ChachaBuf
    Signed-off-by: harodggg <haroldtiansheng@gmail.com>

---
+++ chacha20/README.md
@@ -0,0 +1,5 @@
+# Implementation process
+1.
+2. 
+3. 
+4. 
--- chacha20/src/main.rs
+++ chacha20/src/main.rs
@@ -1,3 +1,9 @@
+#[derive(Debug)]
+enum ChachaBuf{
+    U([u32;16]),   
+    C([u8;64])
+}
 fn main() {
-    println!("Hello, world!");
+    let chachabuf_test: ChachaBuf = ChachaBuf::U([0;16]);
+    println!("Hello, world!{:?}",chachabuf_test);
 }
