1c6e8018d475d895d987a72ed9770518051400ec
harodggg <haroldtiansheng@gmail.com>
2024-04-13 17:11:01 +0800

    feat: add Round trait
    Signed-off-by: harodggg <haroldtiansheng@gmail.com>

--- chacha20/src/main.rs
+++ chacha20/src/main.rs
@@ -3,6 +3,12 @@ enum ChachaBuf{
     U([u32;16]),   
     C([u8;64])
 }
+
+trait Round { 
+    fn quarter_round(a: [u8;4],b: [u8;4],c: [u8;4],d: [u8;4]){ 
+        unimplemented!();
+    }
+}
 fn main() {
     let chachabuf_test: ChachaBuf = ChachaBuf::U([0;16]);
     println!("Hello, world!{:?}",chachabuf_test);
